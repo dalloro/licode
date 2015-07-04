b1498d68780cbb262e98069586c49b3a08e0cab6
Livio <dalloro@gmail.com>
2015-07-04 16:25:52 -0400

    adding utility script to setup the rmeotes

---
+++ setup_git_remotes.sh
@@ -0,0 +1,6 @@
+git remote add licode-nuve git@heroku.com:licode-nuve.git
+git remote add licode-controller git@heroku.com:licode-controller.git
+git remote add licode-agent git@heroku.com:licode-agent.git
+git remote add upstream https://github.com/ging/licode.git
+
+
